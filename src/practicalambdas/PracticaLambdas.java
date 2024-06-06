cc147c05334e3b19cc9f1eba6010962064937c99
Lycan66 <cardonarinconsantiagoandres@gmail.com>
2024-06-06 18:42:18 -0500

    primer commit

---
+++ src/practicalambdas/PracticaLambdas.java
@@ -0,0 +1,34 @@
+package practicalambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+
+public class PracticaLambdas {
+
+    public static void main(String[] args) {
+        
+        /*EJERCICIOS PRACTICOS DE LAMBDAS*/
+        //List<String> strings = List.of("Ojo");
+        //int[] enteros = {5,12,30,4,7,1};
+        
+        //var promedio = Arrays.stream(enteros).average().getAsDouble();
+        //int mayor = Arrays.stream(enteros).max().getAsInt();
+        //int suma = Arrays.stream(enteros).sum();
+        //strings.stream().filter(c -> c.startsWith("A")).forEach(System.out::println);
+        //strings.stream().map(s -> s.toUpperCase()).forEach(System.out::println);
+        //Arrays.stream(enteros).sorted().forEach(System.out::println);
+        //Arrays.stream(enteros).map(n -> n*2).forEach(System.out::println);
+        //String cadena = strings.stream().reduce((str1,str2) -> str1.concat(", "+str2)).get();
+        //Optional<String> cadena = strings.stream().filter(s -> s.length() > 5).findFirst();
+        //Arrays.stream(enteros).map(n -> n*n).forEach(System.out::println);
+        //var conteo = strings.stream().filter(s -> s.length() > 3).count();
+                
+        
+        //System.out.println(conteo);
+        
+        
+    }
+    
+}
